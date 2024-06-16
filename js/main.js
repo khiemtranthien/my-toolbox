4f9fa564ba050fdb9a51ff51f5ca5a1277a3edfe
Khiem Tran <khiemtranthien1411@gmail.com>
2024-06-16 13:56:25 +0700

    Add function to calculate

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js');
   }
+}
+
+function calculate() {
+  var base = $("#base").val();
+  var months = $("#months").val();
+  var logistic = $("#logistic").val();
+  var margin = $("#margin").val();
+
+  var result = base * months / 12 * logistic * margin;
+
+  console.log(base);
+  console.log(months);
+  console.log(logistic);
+  console.log(margin);
+  console.log(result);
+
+  $("#finalAmount").text(result.toFixed(1));
 }
